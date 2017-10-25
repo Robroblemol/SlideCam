8079344756ff8492b9b7377950bf9c606959114b
Raziel <robroblemol>
2017-10-24 23:28:56 -0500

    get text of txf1

--- SlideCamGUI/Gui.pde
+++ SlideCamGUI/Gui.pde
@@ -19,12 +19,12 @@ void createControlGruop( ) {
   bStart = new GButton(this,80,130,100,35,"Ejecutar");
   bStart.fireAllEvents(true);//Habilitamos toda las clases dissparos
 
-  lbTipeVehicule = new GLabel(this, 43, 24, 159, 25);
-  lbTipeVehicule.setText("Slide Cam Control");
+  lbTipeVehicule = new GLabel(this, 73, 5, 159, 25);
+  lbTipeVehicule.setText("Control Slide Cam");
 
-  cbx=new GCheckbox(this,5,5,110,40,"Antiorario");
+  cbx=new GCheckbox(this,5,15,110,40,"Antiorario");
 
-  txf1 = new GTextField(this, 10, 50, 60, 20);
+  txf1 = new GTextField(this, 10, 60, 60, 20);
   txf1.tag = "txf1";
   txf1.setPromptText("Set Steps");
 
@@ -32,6 +32,9 @@ void createControlGruop( ) {
 public void handleButtonEvents(GButton button, GEvent event) {
   if(button==bStart&&event==GEvent.PRESSED){
     println("Me presionaron!! ");
+    if(flag_Dir){
+      
+    }
   }
 }
 public void handleToggleControlEvents(GToggleControl option, GEvent event) {
@@ -40,5 +43,10 @@ public void handleToggleControlEvents(GToggleControl option, GEvent event) {
       flag_Dir = false;
     else  flag_Dir = true;
     println("Me Selecionaron !! ");
+  }
+}
+public void handleTextEvents(GEditableTextControl textcontrol, GEvent event) {
+  if(textcontrol == txf1){
+    println(txf1.getText());
   }
 }
--- SlideCamGUI/build/source/SlideCamGUI.java
+++ SlideCamGUI/build/source/SlideCamGUI.java
@@ -57,12 +57,12 @@ public void createControlGruop( ) {
   bStart = new GButton(this,80,130,100,35,"Ejecutar");
   bStart.fireAllEvents(true);//Habilitamos toda las clases dissparos
 
-  lbTipeVehicule = new GLabel(this, 43, 24, 159, 25);
-  lbTipeVehicule.setText("Slide Cam Control");
+  lbTipeVehicule = new GLabel(this, 73, 5, 159, 25);
+  lbTipeVehicule.setText("Control Slide Cam");
 
-  cbx=new GCheckbox(this,5,5,110,40,"Antiorario");
+  cbx=new GCheckbox(this,5,15,110,40,"Antiorario");
 
-  txf1 = new GTextField(this, 10, 50, 60, 20);
+  txf1 = new GTextField(this, 10, 60, 60, 20);
   txf1.tag = "txf1";
   txf1.setPromptText("Set Steps");
 
@@ -70,6 +70,9 @@ public void createControlGruop( ) {
 public void handleButtonEvents(GButton button, GEvent event) {
   if(button==bStart&&event==GEvent.PRESSED){
     println("Me presionaron!! ");
+    if(flag_Dir){
+
+    }
   }
 }
 public void handleToggleControlEvents(GToggleControl option, GEvent event) {
@@ -78,6 +81,11 @@ public void handleToggleControlEvents(GToggleControl option, GEvent event) {
       flag_Dir = false;
     else  flag_Dir = true;
     println("Me Selecionaron !! ");
+  }
+}
+public void handleTextEvents(GEditableTextControl textcontrol, GEvent event) {
+  if(textcontrol == txf1){
+    println(txf1.getText());
   }
 }
   public void settings() {  size(250,200 ); }
